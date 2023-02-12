4dbf1d3b3c52f167cb5c851f7e5bbc15d4ecb09d
John Ayling <johnayling@yahoo.com>
2023-02-11 21:27:22 -0600

    init

---
+++ scripts/content-script.js
@@ -0,0 +1,122 @@
+document.addEventListener("click", function(e) {
+    const body = document.getElementsByTagName('body')[0];
+    let x = e.clientX;
+    let y = e.clientY;
+    const size = 500;
+
+    const canvas = document.createElement('canvas');
+
+    canvas.style.position = 'absolute';
+    canvas.style.top = `${y - size}px`;
+    canvas.style.left = `${x - size / 2}px`;
+    canvas.style.height = `${size}px`;
+    canvas.style.width = `${size}px`;
+    canvas.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+
+    body.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+    ctx.height = size;
+    ctx.width = size;
+
+    const gravity = 1;
+
+    const particals = [
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+        {
+            posX: 150,
+            posY: 150,
+            vx: -10 + Math.random() * 20,
+            vy: -5 - Math.random() * 10,
+            color: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+        },
+    ];
+
+    var audio = new Audio('https://drive.google.com/file/d/1VZ2puo-M6LWqv93vxtPGXPL8zN3IdVYD/view?usp=sharing');
+    audio.play();
+
+    const interval = setInterval(() => {
+        particals.forEach((partical, index) => {
+            ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+            ctx.fillRect(0, 0, size, size);
+
+            ctx.beginPath();
+            ctx.fillStyle = partical.color;
+            // After setting the fill style, draw an arc on the canvas
+            ctx.arc(partical.posX, partical.posY, 1, 0, Math.PI*2, true);
+            ctx.closePath();
+            ctx.fill();
+
+            particals[index].posX += particals[index].vx;
+            particals[index].posY += particals[index].vy;
+            particals[index].vy += gravity;
+        });
+    }, 20)
+
+    setTimeout(() => {
+        clearInterval(interval);
+        canvas.remove();
+    }, 500);
+}, false);
